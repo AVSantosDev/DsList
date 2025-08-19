1660e048d6ae19e0a3d56391bbd4e9837834f42c
Andre <andrev0795@hotmail.com>
2025-08-18 23:45:50 -0300

    Game seed

--- src/main/java/com/DevSuperior/dslist/entities/Game.java
+++ src/main/java/com/DevSuperior/dslist/entities/Game.java
@@ -5,29 +5,32 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb-game")
+@Table(name = "tb_game")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
-    @Column(name = "Game_year")
+    private Double score;
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
-    private Double score;
+    private String platforms;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {}
 
-    public Game(long id, String title, Integer year, String genre, String platform, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game(long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -66,12 +69,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
